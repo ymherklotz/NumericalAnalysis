dfb39419fcc36003395c807ea01db40650a7de4b
Yann Herklotz <ymherklotz@gmail.com>
2017-02-09 10:46:51 +0000

    Finished making most plots

--- Part 3/scripts/get_function.m
+++ Part 3/scripts/get_function.m
@@ -6,9 +6,9 @@ if f == 1
     count = 1;
     for i = 0:h:1
         if i < 0.5
-            y(count, 1) = 2*i;
+            y(count) = 2*i;
         else
-            y(count, 1) = 2-2*i;
+            y(count) = 2-2*i;
         end
         count = count + 1;
     end
